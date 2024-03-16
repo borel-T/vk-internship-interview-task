68503c232ff98b9c0f2b8e3b3e373510101c3890
borel-T <borel-T@users.noreply.github.com>
2024-03-16 22:19:18 +0300

    update ageGenerator section

--- src/components/ageGenerator/index.js
+++ src/components/ageGenerator/index.js
@@ -1,67 +1,117 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { namesApi } from "../../api";
+import { FormItem, Input, Button, Spinner } from "@vkontakte/vkui";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import _ from "underscore";
 
 export default function AgeGenerator() {
-  const [name, setName] = useState("");
+  //  validation
+  const schema = yup.object().shape({
+    name: yup.string().matches(/^[a-zA-Z]+$/, "Допускаются только символы"),
+  });
+
+  // form-hook
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+    watch,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  //const [name, setName] = useState("");
   const [age, setAge] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [timeoutId, setTimeoutId] = useState(null);
+  const [cache, setCache] = useState({});
 
-  const handleChange2 = (e) => {
-    const value = e.target.value;
-    setName(value);
+  // watch name-field value
+  const nameField = watch("name");
+  useEffect(() => {
     // clearing the previous timeout
     clearTimeout(timeoutId);
     // cet a new timeout to call the API after 3 seconds
-    const newTimeoutId = setTimeout(() => {
-      generateAge(value, "input");
+    const newTimeoutId = setTimeout(async () => {
+      if (nameField) {
+        await trigger("name"); // Trigger validation for firstName and lastName fields
+        // If validation succeeds, submit the form
+        handleSubmit(onSubmit)();
+      }
     }, 3000);
 
     // update the timeoutId state
     setTimeoutId(newTimeoutId);
-  };
+  }, [nameField]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    generateAge(name, "button");
-  };
-
-  const generateAge = (userName, y) => {
-    // call fetchData function when inputValue changes
-    if (name !== "" && userName !== name) {
-      fetch(`${namesApi.names}?name=${userName}`)
+  // generate name
+  const apiCall = (param) => {
+    if (!cache[param]) {
+      // load
+      setIsPending(true);
+      // api call
+      fetch(`${namesApi.names}?name=${param}`)
         .then((response) => response.json())
         .then((data) => {
-          // update age
-          setAge(data.age);
-          // stop loading
-          setIsPending(false);
+          // update
+          setAge(data.age ? data.age : 0);
+          setCache((prev) => ({ ...prev, [param]: data.age }));
         })
-        .catch((error) => console.error("Error fetching age:", error));
+        .catch((error) => console.error("Error fetching age:", error))
+        .finally(() => {
+          setIsPending(false);
+        });
     } else {
-      console.log("no calls");
+      setAge(cache[param]);
     }
   };
 
+  // submit method
+  const onSubmit = (data, e) => {
+    if (e) e.preventDefault();
+    apiCall(data.name);
+  };
+
   return (
-    <section className="age-generator-section">
-      <form className="form" onSubmit={handleSubmit}>
-        <input
-          className="my-text-input"
-          type="text"
-          value={name}
-          onChange={handleChange2}
-          placeholder="Enter your name"
-        />
-        <button
-          className="my-button"
-          type="submit"
-          disabled={!name || isPending}
-        >
-          {isPending ? "Fetching..." : "Submit"}
-        </button>
+    <section className="form-wrap">
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <FormItem top="Текст" htmlFor="name">
+          <VkInput
+            // value={name}
+            name="name"
+            {...register("name")}
+            // onChange={handleChange}
+            placeholder="введите свое имя"
+          />
+          {errors?.name?.message && (
+            <span className="form-error">{errors.name?.message}</span>
+          )}
+        </FormItem>
+        <FormItem>
+          {isPending && (
+            <Spinner
+              color="white"
+              size="small"
+              style={{ marginRight: "5px" }}
+            />
+          )}
+          {!isPending && (
+            <Button className="my-button" type="submit" disabled={!nameField}>
+              Угадай свой возраст
+            </Button>
+          )}
+        </FormItem>
       </form>
-      {age && <p>Predicted age: {age}</p>}
+      {age !== null && <p>прогнозируемый возраст : {age}</p>}
     </section>
   );
 }
+
+// vk-inpuot
+// Wrap the functional component with React.forwardRef()
+const VkInput = React.forwardRef((props, ref) => (
+  <Input {...props} getRootRef={ref} />
+));
