8a49f7cfc70b6624e02af60f19f55354f8d37ce9
borel-T <borel-T@users.noreply.github.com>
2024-03-16 17:49:17 +0300

    updated cat-fact sub section

--- src/components/catfact/index.js
+++ src/components/catfact/index.js
@@ -1,40 +1,74 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { catsApi } from "../../api";
+import { FormItem, Input, Button, Spinner } from "@vkontakte/vkui";
 
 export default function CatFact() {
+  //  consts
+  const textFieldRef = useRef(null);
+
+  // states
   const [userInput, setUserInput] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const textFieldRef = useRef();
-
-  const fetchCatFact = () => {
+  // get facts
+  const fetchCatFact = async () => {
+    setLoading(true);
     fetch(catsApi.catsFacts)
       .then((response) => response.json())
       .then((data) => {
         // Update the cat fact
-        console.log(data.fact);
         setUserInput(data.fact);
-        textFieldRef.current.focus();
+        setSubmitted(true);
       })
-      .catch((error) => console.error("Error fetching cat fact:", error));
+      .catch((error) => console.error("Error fetching cat fact:", error))
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
-  const handleClick = (e) => {
+  // places cursor after the first word of the text-input-field
+  const placeCursorAfterTextFieldFirstWord = (text) => {
+    const firstSpace = text.indexOf(" ");
+    const newPosition = firstSpace >= 0 ? firstSpace + 1 : text.length;
+    textFieldRef.current.setSelectionRange(newPosition, newPosition);
+    textFieldRef.current.focus();
+    setSubmitted(false);
+  };
+
+  // handle submit
+  const handleSubmit = async (e) => {
     e.preventDefault();
     fetchCatFact();
   };
 
+  useEffect(() => {
+    if (userInput !== "") {
+      placeCursorAfterTextFieldFirstWord(userInput);
+    }
+  }, [submitted]);
+
   return (
-    <div className="catfact">
-      <input
-        className="my-text-input"
-        type="text"
-        value={userInput}
-        onChange={(e) => setUserInput(e.target.value)}
-        ref={textFieldRef}
-      />
-      <button className="my-button" onClick={handleClick}>
-        Button
-      </button>
-    </div>
+    <form className="form-wrap">
+      <FormItem top="Текст" htmlFor="name" onSubmit={handleSubmit}>
+        <Input
+          className="my-text-input"
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          getRef={textFieldRef}
+        />
+      </FormItem>
+      <FormItem>
+        {loading && (
+          <Spinner color="white" size="small" style={{ marginRight: "5px" }} />
+        )}
+        {!loading && (
+          <Button type="submit" onClick={handleSubmit}>
+            {"Нажмите, чтобы узнать больше о кошках."}
+          </Button>
+        )}
+      </FormItem>
+    </form>
   );
 }
