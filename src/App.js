da39bf1f1166b1c9f70557ac509a8a0a1497821d
borel-T <borel-T@users.noreply.github.com>
2024-03-16 22:19:42 +0300

    updated app css files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import "./App.css";
+import React from "react";
 import CatFact from "./components/catfact";
 import AgeGenerator from "./components/ageGenerator";
+import {
+  View,
+  Panel,
+  PanelHeader,
+  Group,
+  Cell,
+  PanelHeaderBack,
+} from "@vkontakte/vkui";
+import "@vkontakte/vkui/dist/vkui.css";
+import {
+  Icon24StarCircleFillGreen,
+  Icon28RobotOutline,
+} from "@vkontakte/icons";
 
 function App() {
+  const [activePanel, setActivePanel] = React.useState("panel1");
+
   return (
-    <div className="vk-app">
-      <CatFact />
-      <AgeGenerator />
-    </div>
+    <View activePanel={activePanel}>
+      {/* Default view */}
+      <Panel id="panel1">
+        <PanelHeader>More</PanelHeader>
+        <Group>
+          <Cell
+            expandable="auto"
+            before={<Icon24StarCircleFillGreen />}
+            onClick={() => setActivePanel("panel2")}
+          >
+            Cat Facts
+          </Cell>
+          <Cell
+            expandable="auto"
+            before={<Icon28RobotOutline />}
+            onClick={() => setActivePanel("panel3")}
+          >
+            Age Generator
+          </Cell>
+        </Group>
+      </Panel>
+      {/* Cats-Fact */}
+      <Panel id="panel2" centered>
+        <PanelHeader
+          delimiter="spacing"
+          before={<PanelHeaderBack onClick={() => setActivePanel("panel1")} />}
+        >
+          Cats Fact
+        </PanelHeader>
+        <CatFact />
+      </Panel>
+      {/* Age-Generator */}
+      <Panel id="panel3" centered>
+        <PanelHeader
+          before={<PanelHeaderBack onClick={() => setActivePanel("panel1")} />}
+        >
+          Age-Generator
+        </PanelHeader>
+        <AgeGenerator />
+      </Panel>
+    </View>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import bridge from "@vkontakte/vk-bridge";
+import { ConfigProvider, AdaptivityProvider } from "@vkontakte/vkui";
 
 bridge.send("VKWebAppInit", {});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ConfigProvider>
+    <AdaptivityProvider>
+      <App />
+    </AdaptivityProvider>
+  </ConfigProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
